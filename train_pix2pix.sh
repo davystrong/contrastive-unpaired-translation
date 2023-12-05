b9749be5bca1cd5176e2286c1af2bdafa1d4a218
David Armstrong <david.armstrong.br@gmail.com>
2023-12-05 13:54:40 +0000

    Added Pix2Pix model and training script Changed timeout

--- train_pix2pix.sh
+++ train_pix2pix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=train_cycle
-#SBATCH --output="/users/40390351/contrastive-unpaired-translation/train_cycle_output.txt"
+#SBATCH --job-name=train_pix2pix
+#SBATCH --output="/users/40390351/contrastive-unpaired-translation/train_pix2pix_output.txt"
 #SBATCH --mail-user=darmstrong34@qub.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --ntasks=1
@@ -19,4 +19,4 @@ ee=/mnt/scratch2/users/40390351
 # cd $dd/Experiments/23-2-2
 cd "$dd/contrastive-unpaired-translation"
 ln -fs "$dd/ml_notebook/Experiments/Shared/23-11-23.sif" environment.sif
-apptainer exec --nv --bind $dd:$dd --bind $ee:$ee ./environment.sif bash -c 'python3 -m pip install -r requirements.txt && python3 train.py --dataroot ./datasets/mouse --model cycle_gan --name mouse_cycle'
+apptainer exec --nv --bind $dd:$dd --bind $ee:$ee ./environment.sif bash -c 'python3 -m pip install -r requirements.txt && python3 train.py --dataroot ./datasets/mouse --model pix2pix --name mouse_pix2pix --dataset_mode unaligned --lambda_L1 0'
