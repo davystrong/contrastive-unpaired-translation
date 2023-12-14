38305bb51c50ffe16b1ea1cc2657e960f9b01814
David Armstrong <david.armstrong.br@gmail.com>
2023-12-14 12:43:51 +0000

    Testing

--- models/tracking_instance_norm.py
+++ models/tracking_instance_norm.py
@@ -3,12 +3,9 @@ import torch.nn.functional as F
 
 class _TrackingInstanceNorm(instancenorm._InstanceNorm):
     def _apply_instance_norm(self, input):
-        F.instance_norm(
-            input, self.running_mean, self.running_var, self.weight, self.bias,
-            True, self.momentum, self.eps)
         return F.instance_norm(
             input, self.running_mean, self.running_var, self.weight, self.bias,
-            False, self.momentum, self.eps)
+            self.training or not self.track_running_stats, self.momentum, self.eps)
 
 class TrackingInstanceNorm2d(_TrackingInstanceNorm):
     r"""Applies Instance Normalization over a 4D input (a mini-batch of 2D inputs
