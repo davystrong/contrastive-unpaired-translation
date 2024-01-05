0fdc9ef0c106e4161150ea6f89dc367713faf6f7
David Armstrong <david.armstrong.br@gmail.com>
2024-01-05 14:35:33 +0000

    Added chop model

---
+++ train_chop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=train_chop
+#SBATCH --output="/users/40390351/contrastive-unpaired-translation/train_chop_output.txt"
+#SBATCH --mail-user=darmstrong34@qub.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=k2-gpu,gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=24:00:00
+
+module load apps/apptainer/1.1.2
+# module load nvidia-cuda
+# module load libs/nvidia-cuda/11.7.0/bin
+
+dd=/users/40390351
+ee=/mnt/scratch2/users/40390351
+# cd $dd/Experiments/23-2-2
+cd "$dd/contrastive-unpaired-translation"
+ln -fs "$dd/ml_notebook/Experiments/Shared/23-11-23.sif" environment.sif
+apptainer exec --nv --bind $dd:$dd --bind $ee:$ee ./environment.sif bash -c 'python3 -m pip install -r requirements.txt && python3 train.py --dataroot ./datasets/mouse --name mouse_Chop --CUT_mode CUT --model chop --gen_splits 1 --display_id -1'
